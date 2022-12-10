82380fb14e8ed437a11e0a047f337b06c6136a9b
Andriii888 <106496840+Andriii888@users.noreply.github.com>
2022-12-10 23:40:17 +0300

    addwithoutscroll

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import Notiflix from 'notiflix';
 const newAPIServis = new APIServis();
 
 
-
 Notiflix.Notify.init({
     borderRadius: '10px',
     position: 'center-center',
@@ -19,80 +18,105 @@ Notiflix.Notify.init({
     backOverlayColor: 'rgba(238,191,49,0.2)',
   },
 });
-const galleryBox = new SimpleLightbox('.gallery a', {
+new SimpleLightbox('.photo-card a', {
       scrollZoom: false,
     captionsData: "alt",
     captionDelay: 250,
 
 });
-
 const inputValueRef = document.querySelector('#search-form');
 const galleryBoxRef = document.querySelector('.gallery');
 const loadMoreBtn = document.querySelector('.load-more');
+// let elemForInfinitScrole = document.querySelector('.container');
+// let infScroll = new InfiniteScroll( elemForInfinitScrole, {
+//   // options
+// //   path: '.pagination__next',
+// //   append: '.post',
+// //   history: false,
+// });
+
 
 
 inputValueRef.addEventListener('submit', onSearch);
 loadMoreBtn.addEventListener('click', loadMoreImg);
 
-function onSearch(e) {
-    e.preventDefault();
-    clearContent();
-    loadMoreBtn.disabled = false;
-     const {
-    elements: { searchQuery }
-  } = e.currentTarget;
-    newAPIServis.inpValue = searchQuery.value;
-    newAPIServis.resetPage();
+async function onSearch(e) {
+    try {
+        e.preventDefault();
+        clearContent();
+        loadMoreBtn.disabled = false;
+        const {
+            elements: { searchQuery }
+        } = e.currentTarget;
+        newAPIServis.inpValue = searchQuery.value;
+        newAPIServis.resetPage();
  
-    newAPIServis.fetchPosts().then(({ data }) => {
-        let totalHits = data.totalHits;
-        Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
-     newAPIServis.pageValue += 1;
+       await newAPIServis.fetchPosts().then(({ data }) => {
+           let totalHits = data.totalHits;
+            if (data.hits.length === 0) {
+        Notiflix.Notify.failure(`Sorry, there are no images matching your search query. Please try again.`);
+    }
+           if (data.hits.length > 0) {
+               Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
+           }           
+            newAPIServis.pageValue += 1;
 
-        createPosts(data);
-    })
-        .catch((error) => {
+           createPosts(data);
+           createSmoothScroll();
+
+           //////////////////////////////////////////
+        
+///////////////////////////////////
+        })
+    }
+        catch (error) {
             console.log(error);
              Notiflix.Notify.failure(`Sorry, there are no images matching your search query. Please try again.`);
-        });
+        };
 
 };
 
 
 function createPosts(val) {
-    if (val.hits.length === 0) {
-        Notiflix.Notify.failure(`Sorry, there are no images matching your search query. Please try again.`);
-    }
-    if (!val.totalHits) {
-        Notiflix.Notify.failure(`We're sorry, but you've reached the end of search results.`);
-    }
-
     const elementPosts = val.hits.map(
-        ({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
-        return `<div class="photo-card post">
-    <a>
-    <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-    </a>
-    <div class="info">
+        ({ webformatURL, largeImageURL, tags, likes, views, comments, downloads },index) => {
+            return `<div class="photo-card post">
+            <a href="${largeImageURL}">
+        <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+        </a>
+        <div class="info">
         <p class="info-item">
-            <b>Likes:${likes}</b>
+            <b>Likes:</br>
+            ${likes}</b>
         </p>
         <p class="info-item">
-            <b>Views:${views}</b>
+            <b>Views:</br>
+            ${views}</b>
         </p>
         <p class="info-item">
-            <b>Comments:${comments}</b>
+            <b>Comments:</br>
+            ${comments}</b>
         </p>
         <p class="info-item">
-            <b>Downloads:${downloads}</b>
+            <b>Downloads:</br>
+            ${downloads}</b>
         </p>
     </div>
 </div>`
-    }
+          
+}
       
     ).join("");
-    // var lightbox = $('.gallery a').simpleLightbox({ /* options */ });
-    return galleryBoxRef.insertAdjacentHTML('beforeend', elementPosts);
+   
+    // lightbox.refresh();
+    
+     galleryBoxRef.insertAdjacentHTML('beforeend', elementPosts);
+   return new SimpleLightbox('.photo-card a', {
+      scrollZoom: false,
+    captionsData: "alt",
+    captionDelay: 250,
+
+});
 };
 
 function clearContent() {
@@ -100,18 +124,44 @@ function clearContent() {
 };
 
 
-function loadMoreImg() {
-    newAPIServis.fetchPosts().then(({ data }) => {
-                newAPIServis.pageValue += 1;
+async function loadMoreImg() {
+    try {
+        await newAPIServis.fetchPosts().then(({ data }) => {
+            newAPIServis.pageValue += 1;
 
-        createPosts(data);
-    })
-        .catch((error) => {
+            createPosts(data);
+            createSmoothScroll();
+        })
+    }
+        catch (error)  {
             console.log(error.message);
             if (error.message = 400) {
-             Notiflix.Notify.failure(`We're sorry, but you've reached the end of search results.`);   
+            return Notiflix.Notify.failure(`We're sorry, but you've reached the end of search results.`);   
             }
              Notiflix.Notify.failure(`Sorry, there are no images matching your search query. Please try again.`);
-        });
-}
+        };
+};
 
+function createSmoothScroll() {
+   const { height: cardHeight } = document.querySelector(".gallery").firstElementChild.getBoundingClientRect();
+           window.scrollBy({ top: cardHeight * 0.5, behavior: "smooth", });
+};
+/////////////////////////////////////////////////////////
+// async function loadNextPage() {
+//     try {
+//         await newAPIServis.fetchPosts().then(({ data }) => {
+//             newAPIServis.pageValue += 1;
+
+//             createPosts(data);
+//             createSmoothScroll();
+//         })
+//     }
+//         catch (error)  {
+//             console.log(error.message);
+//             if (error.message = 400) {
+//             return Notiflix.Notify.failure(`We're sorry, but you've reached the end of search results.`);   
+//             }
+//              Notiflix.Notify.failure(`Sorry, there are no images matching your search query. Please try again.`);
+//         };
+// };
+// infScroll.loadNextPage();
